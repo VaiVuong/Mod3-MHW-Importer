3597a427f02a7245e8ffff08ba087974469d9792
Chris V <creevue@gmail.com>
2025-09-20 16:19:25 -0400

    Changed import/export reference for 4.5.3 blender testing.

--- __init__.py
+++ __init__.py
@@ -4,8 +4,6 @@ Created on Wed Mar  6 13:38:47 2019
 
 @author: AsteriskAmpersand
 """
-#from .dbg import dbg_init
-#dbg_init()
 
 bl_info = {
     "name": "MHW Mod3 Model Importer",
@@ -14,7 +12,7 @@ bl_info = {
     "location": "File > Import-Export > Mod3/MHW",
     "version": (2,1,0)
 }
- 
+
 import bpy
 
 from .operators.mod3properties import symmetricPair
@@ -23,21 +21,24 @@ from .operators.mod3export import ExportMOD3
 from .operators.mod3import import menu_func_import as mhw_model_menu_func_import
 from .operators.mod3export import menu_func_export as mhw_model_menu_func_export
 
+# 2.8 vs 4.5.3 changes
+# INFO_MT_file_import (old) -> TOPBAR_MT_file_import (new)
+# INFO_MT_file_export (old) -> TOPBAR_MT_file_export (new)
+# Replaced all old references with new 9/20/2025 -VV
 def register():
     bpy.utils.register_class(ImportMOD3)
-    bpy.utils.register_class(ExportMOD3)    
-    bpy.types.INFO_MT_file_import.append(mhw_model_menu_func_import)
-    bpy.types.INFO_MT_file_export.append(mhw_model_menu_func_export)
+    bpy.utils.register_class(ExportMOD3)
+    bpy.types.TOPBAR_MT_file_import.append(mhw_model_menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(mhw_model_menu_func_export)
     bpy.types.Object.MHW_Symmetric_Pair = symmetricPair
-    
+
 
 def unregister():
     del bpy.types.Object.MHW_Symmetric_Pair
     bpy.utils.unregister_class(ImportMOD3)
     bpy.utils.unregister_class(ExportMOD3)
-    bpy.types.INFO_MT_file_import.remove(mhw_model_menu_func_import)
-    bpy.types.INFO_MT_file_export.remove(mhw_model_menu_func_export)
-    
+    bpy.types.TOPBAR_MT_file_import.remove(mhw_model_menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(mhw_model_menu_func_export)
     #del bpy.types.Object.MHWSkeleton
 
 if __name__ == "__main__":
